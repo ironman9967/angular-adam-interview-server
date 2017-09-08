086dcb4bc8a63a3cb1efd3f26d6d26c7994c1105
Tyson Hester <ironman9967@gmail.com>
2017-09-08 15:02:59 -0400

    ...

--- src/test.js
+++ src/test.js
@@ -1,7 +1,7 @@
 
 const io = require('socket.io-client')
 
-const socket = io('http://localhost:3000')
+const socket = io('http://localhost:9000')
 
 socket.on('connect', () => console.log('connected'))
 
