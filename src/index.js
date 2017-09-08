807ece31431b7c2adb6ff9c13ca43a6c9d0d265b
Tyson Hester <ironman9967@gmail.com>
2017-09-08 14:55:53 -0400

    init commit

---
+++ src/index.js
@@ -0,0 +1,39 @@
+
+const io = require('socket.io')
+const uuidv4 = require('uuid/v4')
+
+const server = new io()
+
+const messages = [
+  'this is a test entry',
+  'hello, world!',
+  'another message',
+  'yo'
+]
+
+const port = 9000
+
+let rnd = Math.random()
+
+server.on('connection', (socket) => {
+  socket.logEmitter = setInterval(() => {
+    rnd = Math.random()
+    socket.emit('msg', {
+      id: uuidv4(),
+      message: messages[Math.floor(rnd * 4)]
+    })
+  }, rnd * 3 * 1000)
+  socket.emit('remove', msg => {
+    console.log(msg.id, 'removed')
+  })
+})
+
+server.on('disconnect', (socket) => {
+  if (socket.logEmitter) {
+    clearInterval(socket.logEmitter)
+  }
+})
+
+server.listen(port)
+
+console.log('server up on', port)
